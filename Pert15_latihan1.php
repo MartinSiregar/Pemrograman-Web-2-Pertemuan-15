f86b7072d7815d999c8b5f48f5e3e63bfd303bf1
Martin Siregar <martin@arelosoft.com>
2021-05-27 21:18:42 +0700

    commit

---
+++ Pert15_latihan1.php
@@ -0,0 +1,10 @@
+<?php
+$value = 'wkwk';
+$value2 = 'Martin Siregar';
+setcookie("username", $value);
+setcookie("namalengkap", $value2, time()+3600); /* expire in 1
+hour */
+echo "<h1>Ini halaman pengesetan cookie</h1>";
+echo "<h2>Klik <a href='Pert15_latihan2.php'>di sini</a> untuk
+pemeriksaan cookies</h2>";
+?>
